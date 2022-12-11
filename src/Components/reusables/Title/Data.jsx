a2870bcd8c46d01e1f85c43e09afc8c1216209e4
RamFarid <workprojects22@gmail.com>
2022-12-11 21:30:20 +0200

    Add time/seasons to single title page

--- src/Components/reusables/Title/Data.jsx
+++ src/Components/reusables/Title/Data.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Request } from '../../../APIs/apiMain'
+import PopupMovieTime from '../PopUpInfoModels/PopupMovieTime'
 
 function Data({ data }) {
   const title = Request.titleGenerator(data)
+  const seasons = data?.seasons?.length
+  const time = data?.runtime
   const rank =
     data?.release_date?.slice(0, 4) || data?.first_air_date?.slice(0, 4)
   return (
     <div className='data'>
       <h1 className='title'>{title}</h1>
       <div className='bottom'>
-        {/*<img
-          src='https://cdn-icons-png.flaticon.com/512/156/156855.png'
-          alt=''
-        /> */}
         <span className='rank'>{rank}</span>
         <span className='rank maturity'>Not sure</span>
+        <PopupMovieTime seasons={seasons} time={time} />
       </div>
     </div>
   )
