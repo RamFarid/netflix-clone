6360fbc084ce317ba88ad9b7b08d400f009c5b19
RamFarid <workprojects22@gmail.com>
2022-12-11 03:56:06 +0200

    Login page is Responsive!

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -8,7 +8,6 @@ import Footer from '../Components/LoginPage/Footer'
 import { AuthContext } from '../Contexts/AuthContext'
 
 import '../Styles/Login/login.css'
-import { BrowserView, MobileView } from 'react-device-detect'
 function Login() {
   const EMAIL = 'workprojects22@gmail.com'
   const PASSWORD = '123456'
@@ -33,94 +32,53 @@ function Login() {
     }
   }
   return (
-    <main className='Login__Page'>
-      <MobileView>
-        <section>
-          <div className='si-card mobile'>
-            <h2>Sign In</h2>
-            {isValidation && (
-              <div className='validation'>
-                Sorry, we can't find an account with this email address. Please
-                try again or create a new account.
-              </div>
-            )}
-            <EmailInput setEmail={setEmail} />
-            <PasswordInput setPassword={setPassword} />
-            <SigninInput
-              password={password}
-              email={email}
-              handleLogin={handleLogin}
-            />
-            <div className='u-b'>
-              <Rememberme setIsChecked={setIsChecked} />
-              <Link to='' className='footer__item'>
-                Need help?
-              </Link>
+    <main className={'Login__Page'}>
+      <div className='top'>
+        <Link to='/'>
+          <img
+            src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+            alt='Netflix'
+          />
+        </Link>
+      </div>
+      <section>
+        <div className='si-card'>
+          <h2>Sign In</h2>
+          {isValidation && (
+            <div className='validation'>
+              Sorry, we can't find an account with this email address. Please
+              try again or create a new account.
             </div>
-            <div className='n-su-co'>
-              New to Netflix?{' '}
-              <Link to='' className='ca'>
-                Sign up now!
-              </Link>
-            </div>
-            <div className='bot'>
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.{' '}
-              <Link to='' className='ca'>
-                Learn more.
-              </Link>
-            </div>
+          )}
+          <EmailInput setEmail={setEmail} />
+          <PasswordInput setPassword={setPassword} />
+          <SigninInput
+            password={password}
+            email={email}
+            handleLogin={handleLogin}
+          />
+          <div className='u-b'>
+            <Rememberme setIsChecked={setIsChecked} />
+            <Link to='' className='footer__item'>
+              Need help?
+            </Link>
           </div>
-        </section>
-      </MobileView>
-      <BrowserView>
-        <div className='top'>
-          <Link to='/'>
-            <img
-              src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
-              alt='Netflix'
-            />
-          </Link>
+          <div className='n-su-co'>
+            New to Netflix?{' '}
+            <Link to='' className='ca'>
+              Sign up now!
+            </Link>
+          </div>
+          <div className='bot'>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot.{' '}
+            <Link to='' className='ca'>
+              Learn more.
+            </Link>
+          </div>
         </div>
-        <section>
-          <div className='si-card'>
-            <h2>Sign In</h2>
-            {isValidation && (
-              <div className='validation'>
-                Sorry, we can't find an account with this email address. Please
-                try again or create a new account.
-              </div>
-            )}
-            <EmailInput setEmail={setEmail} />
-            <PasswordInput setPassword={setPassword} />
-            <SigninInput
-              password={password}
-              email={email}
-              handleLogin={handleLogin}
-            />
-            <div className='u-b'>
-              <Rememberme setIsChecked={setIsChecked} />
-              <Link to='' className='footer__item'>
-                Need help?
-              </Link>
-            </div>
-            <div className='n-su-co'>
-              New to Netflix?{' '}
-              <Link to='' className='ca'>
-                Sign up now!
-              </Link>
-            </div>
-            <div className='bot'>
-              This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.{' '}
-              <Link to='' className='ca'>
-                Learn more.
-              </Link>
-            </div>
-          </div>
-        </section>
-        <Footer />
-      </BrowserView>
+      </section>
+      <Footer />
     </main>
   )
 }
